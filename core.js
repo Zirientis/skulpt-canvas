473e86808c07e0af26f39896f63156490ba0621b
Zirientis <Zirientis@users.noreply.github.com>
2016-04-27 19:32:50 -0700

    Begin JS command handling

--- core.js
+++ core.js
@@ -17,6 +17,11 @@ _INJ.element = cv;
 _INJ.renderCtx = ctx;
 _INJ.linkUpTimer = setInterval(isLinkReady, 1000);
 _INJ.linkUp = false;
+
+// Configure the JS<->Python link
+var observer = new MutationObserver(mutationCallback);
+var mutationConfig = { childList: true };
+observer.observe(cv, mutationCallback);
 })();
 
 function isLinkReady()
@@ -30,5 +35,41 @@ function isLinkReady()
 		{
 			onLinkReady();
 		}
+	}
+}
+
+function mutationCallback(mutations, observer)
+{
+	if (DEBUG)
+	{
+		console.debug("observer called with " + mutations.length + " records");
+	}
+	for (var mri = 0; mri < mutations.length; mri++)
+	{
+		var mr = mutations[mri].addedNodes;
+		if (DEBUG)
+		{
+			console.debug("record " + mri + ": " + mr.length + " nodes added");
+		}
+		for (var i = 0; i < mr.length; i++)
+		{
+			var mut = mr[i];
+			var cmdstr = mut.textContent;
+			if (DEBUG)
+			{
+				console.debug("recieved command: " + cmdstr);
+				var cmd = cmdstr.split(' ');
+				handleCommand(cmd);
+			}
+		}
+	}
+}
+
+function handleCommand(cmdwargs)
+{
+	var cmdname = cmdwargs[0];
+	if (DEBUG)
+	{
+		console.debug("executing a " + cmdname);
 	}
 }
